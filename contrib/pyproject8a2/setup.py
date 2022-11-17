5aa09b1adefa73fd413c6dd46225f841b06e69df
Oshinko <osnk@renjaku.jp>
2022-11-17 12:18:13 +0900

    Update

--- contrib/pyproject8a2/setup.py
+++ contrib/pyproject8a2/setup.py
@@ -2,15 +2,4 @@ import setuptools
 
 import private_requirements
 
-setuptools.setup(
-    name='pyproject8a2',
-    version='0.0.0',
-    packages=['package'],
-    install_requires=private_requirements.get(),
-    classifiers=[
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: Implementation :: CPython',
-        'Programming Language :: Python :: Implementation :: PyPy'
-    ]
-)
+setuptools.setup(install_requires=private_requirements.get())
